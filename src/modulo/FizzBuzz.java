9ca9aad9f0919c96677321f5c75644de41332f5d
kang.jeremiah <kang.jeremiah@SEMS-259-36.SEMS259.LAB>
2018-03-07 15:45:07 -0800

    idc

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,7 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -26,6 +27,43 @@ package modulo;
 **/
 
 public class FizzBuzz {
+public static void main(String[] args) {
 
+	for(int i = 1; i<=2018;i++) {
+		if(i%3==0&&i%5==0) {
+	System.out.print("fizzbuzz ");
+		}
+	else if(i%3==0){
+		System.out.print("fizz ");
+	}
+	
+	else if(i%5==0){
+		System.out.print("buzz ");
+	
+	}
+	else {
+		System.out.print(i+ " ");
+	}
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
 
