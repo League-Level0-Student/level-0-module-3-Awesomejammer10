778508fe4c2e68be9b9b73e62952b44fbb6f569a
kang.jeremiah <kang.jeremiah@SEMS-259-36.SEMS259.LAB>
2018-05-16 16:30:02 -0700

    forlooppractice

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,23 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+		System.out.println("Single Test One");
+		for (int i = 0; i <= 100; i++) {
+			System.out.println(i);
+
+		}
+		System.out.println("Single Test Two");
+		for (int i = 100; i >= 0; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Single test Three");
+for(int i= 0; i<=100; i+=2) {
+	System.out.println(i);
+}
+System.out.println("Single Test Four");
+for(int i= 1; i<=100; i+=2) {
+	System.out.println(i);
+}
+	}
+
+}
